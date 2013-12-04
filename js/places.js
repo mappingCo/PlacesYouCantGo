f7af02bd53a905f65efbe667f2b345abad2ff4c1
mappingCo <mapping.and.co@gmail.com>
2013-12-04 09:41:38 +0100

    update

--- js/places.js
+++ js/places.js
@@ -10,6 +10,9 @@ var map,
 
 var numFeatures = 10;
 
+var InitialCenter = new L.LatLng(40.7, 30.25);
+
+
 //view control buttons
 $('#go').click(function(){
   $('#back-button').removeClass('hidden');
@@ -46,37 +49,51 @@ var osmlayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   maxZoom: 18,
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 });
+//MapQuest Open Aerial - global coverage is provided at zoom levels 0-11. Zoom Levels 12+ are provided only in the United States)
 var MapQuest = L.tileLayer('http://oatile{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg', {
   attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> &mdash; Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency',
   subdomains: '1234',
   maxZoom:11
 });
-var googleLayer = new L.Google('SATELLITE');
+//var googleLayer = new L.Google('SATELLITE');
 
 
 var baseLayers = {
   "osm": osmlayer,
-  "MapQuest Sat":MapQuest,
-  "Google Sat": googleLayer
+  "MapQuest Sat":MapQuest
+  //"Google Sat": googleLayer
 };
 
-//get geojson data on the map
+function onEachFeature(feature, layer) {
+  layer.on({
+    mouseover:hoverEfect
+  })
+  //layer.bindPopup('<b>'+feature.properties.name + '</b><br />' + feature.properties.lat+', '+ feature.properties.lon+'<br/><img src="'+feature.properties.top+'.png">');
+};
+
+function hoverEfect(e){
+  var layer=e.target;
+  layer.bindPopup('<b class="popupTitle">'+layer.feature.properties.name + '</b><br />' + layer.feature.properties.lat+', '+ layer.feature.properties.lon+'<br/><img width="300px" src="./img/'+layer.feature.properties.top+'.jpg">', {
+    minWidth: 320,
+  }).openPopup();
+};
+
+//get geojson data and creates a layer 
 $.getJSON("./GeoJSON/places.geojson", function(data) {
-  var geojson = L.geoJson(data, {
-    onEachFeature: function (feature, layer) {
-      layer.bindPopup('<b>'+feature.properties.name + '</b><br />' + feature.properties.lat+', '+ feature.properties.lon);
-    }
+  var geojsonLayer = L.geoJson(data, {
+    //The onEachFeature option is a function that gets called on each feature before adding it to a GeoJSON layer. 
+    onEachFeature: onEachFeature
   });
-  map = L.map('PlacesYouCantGo').fitBounds(geojson.getBounds());
+  map = L.map('PlacesYouCantGo').fitBounds(geojsonLayer.getBounds());
 
   L.control.layers(baseLayers).addTo(map);
   osmlayer.addTo(map);
-  geojson.addTo(map);
-  map.setView(new L.LatLng(40.7, 30.25), 2);
+  geojsonLayer.addTo(map);
+  map.setView(InitialCenter, 2);
   
 });
 
-var popupContent;
+
 //zoom a la siguiente localizacion 
 function changeCenter(mapView){
   $.ajax({
@@ -92,7 +109,6 @@ function changeCenter(mapView){
       info_p = data.features[mapView].properties.text;
       top_p = data.features[mapView].properties.top;
       mapViewZoom= data.features[mapView].properties.zoom;
-      popupContent ='<b>'+data.features[mapView].properties.name + '</b><br />' + data.features[mapView].properties.lat+', '+  data.features[mapView].properties.lon
 
     }
 
@@ -101,19 +117,22 @@ function changeCenter(mapView){
 
   var targetlatlng = L.latLng(mapCenterLat, mapCenterLon);
   map.setView(targetlatlng, mapViewZoom);
-  //AwesomeMarkers
-  var cofeeIcon = L.AwesomeMarkers.icon({
-    icon: 'coffee', 
-    markerColor: 'orange', 
-    prefix: 'fa', 
-    iconColor: 'black'
+
+/*  var oneIcon = L.icon({
+    iconUrl: 'Forbidden-icon.png', 
+    iconSize:     [38, 40], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
   });
   L.marker([mapCenterLat,mapCenterLon], {
-    icon: cofeeIcon
-  }).addTo(map);
-  marker.zIndexOffset(1000);
-  marker.riseOnHover(true);
-  marker.bindPopup(popupContent);
+    icon: oneIcon, 
+    //zIndexOffset: 1000, 
+    riseOnHover:true,
+    bindPopup: popupContent
+  }).addTo(map);*/
+
   
 
 
